f612f58e94bcbb22d5cc4d66923979be3f0b1394
Leonard <realcodedevs@gmail.com>
2021-10-23 21:22:06 +0300

    created react-app

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+	return (
+		<div className="bg-red-600 flex items-center justify-center">
+			<div className="card bg-blue-500 px-5 flex items-center justify-center">
+				<img src="images/icon-music.svg" className="" alt="" />
+				<h1>
+					CardklhrkgjhkzdlgzkldjgbCardklhrkgjhkzdlgzkldjgbCardklhrkgjhkzdlgzkldjgb
+				</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
